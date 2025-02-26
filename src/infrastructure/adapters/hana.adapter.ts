d671c0bb54f330c648a570b96960c90137908381
enokyw <enokyws@gmail.com>
2025-02-26 11:58:18 -0500

    purchase comit

--- src/infrastructure/adapters/hana.adapter.ts
+++ src/infrastructure/adapters/hana.adapter.ts
@@ -13,7 +13,6 @@ export class HanaAdapter {
             SELECT
                 COUNT(*) AS "total"
             FROM OPRQ T0
-            INNER JOIN PRQ1 T1 ON T0."DocEntry" = T1."DocEntry"
             INNER JOIN OUBR T2 ON T0."Branch" = T2."Code"
             INNER JOIN OUSR T3 ON T0."Requester" = T3."USER_CODE"
             INNER JOIN OUDP T4 ON T3."Department" = T4."Code"
@@ -29,17 +28,15 @@ export class HanaAdapter {
         if(totalRecords) {
             return {
                 data: await this.hanaDb.query(`
-                    SELECT 
-                        T0."DocNum", 
-                        T0."DocDate", 
-                        T0."DocDueDate",
+                    SELECT
+                        T0."DocNum",
+                        TO_CHAR(T0."DocDate", 'DD-MM-YYYY') AS "DocDate",
+                        TO_CHAR(T0."DocDueDate", 'DD-MM-YYYY') AS "DocDueDate",
                         T0."ReqName",
                         T0."DocCur",
-                        T1."Quantity",
-                        T2."Name" AS "Branch",
-                        T4."Name" AS "Department"
-                    FROM PRQ1 T1  
-                    INNER JOIN OPRQ T0 ON T0."DocEntry" = T1."DocEntry"
+                        T2."Name" AS "Sucursal",
+                        T4."Name" AS "Departamento"
+                    FROM OPRQ T0
                     INNER JOIN OUBR T2 ON T0."Branch" = T2."Code"
                     INNER JOIN OUSR T3 ON T0."Requester" = T3."USER_CODE"
                     INNER JOIN OUDP T4 ON T3."Department" = T4."Code"
@@ -50,7 +47,6 @@ export class HanaAdapter {
                         OR T0."ReqName" LIKE '%${search}%'
                         OR T4."Name" LIKE '%${search}%')` : ''
                     }
-                    ORDER BY T0."DocNum"
                     LIMIT ${limit} OFFSET ${(page - 1) * limit}
                 `),
                 meta: {
