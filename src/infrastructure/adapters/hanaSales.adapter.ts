41fea7eb5ffbe6c0a7f9e5e906b4e4c4a600107b
enokyw <enokyws@gmail.com>
2025-02-27 13:15:51 -0500

    updated sales order query

--- src/infrastructure/adapters/hanaSales.adapter.ts
+++ src/infrastructure/adapters/hanaSales.adapter.ts
@@ -7,41 +7,43 @@ export class HanaSalesAdapter {
     constructor(private readonly hanaService: HanaService) { }
 
     async QuerySaleOrderByDocNum(DocNum: number) {
-        const saleOrder = await this.hanaService.query(`
-            SELECT 
-                T0."DocNum",
-                T0."CardCode",
-                T0."CardName",
-                T0."DocDate",
-                T0."DocDueDate",
-                T0."TaxDate",
-                T0."DocStatus",
-                T0."NumAtCard",
-                T1."SeriesName" || '-' || T0."DocNum" AS "Serie",
-                T2."LineNum",
-                T2."ItemCode",
-                T2."Dscription",
-                T2."Quantity",
-                T2."OpenQty",
-                T2."DelivrdQty",
-                T2."ShipDate",
-                T2."ShipToCode",
-                T2."ShipToDesc"
-            FROM ORDR T0
-            INNER JOIN NNM1 T1 ON T0."Series" = T1."Series"
-            LEFT JOIN RDR1 T2 ON T0."DocEntry" = T2."DocEntry"
-            WHERE T0."DocNum" = ${DocNum}
-            ORDER BY T0."DocNum" DESC
-        `) as {[key:string]:any}[];
-
-        const whs = await this.hanaService.query(`
-            SELECT
-                T0."Address",
-                T0."Street"
-            FROM CRD1 T0
-            INNER JOIN ORDR T1 ON T0."CardCode" = T1."CardCode"
-            WHERE T1."DocNum" = ${DocNum}
-        `)
+        const [saleOrder, whs] = await Promise.all([
+            this.hanaService.query(`
+                SELECT 
+                    T0."DocNum",
+                    T0."CardCode",
+                    T0."CardName",
+                    T0."DocDate",
+                    T0."DocDueDate",
+                    T0."TaxDate",
+                    T0."DocStatus",
+                    T0."NumAtCard",
+                    T1."SeriesName" || '-' || T0."DocNum" AS "Serie",
+                    T2."LineNum",
+                    T2."ItemCode",
+                    T2."Dscription",
+                    T2."Quantity",
+                    T2."OpenQty",
+                    T2."DelivrdQty",
+                    T2."ShipDate",
+                    T2."ShipToCode",
+                    T2."ShipToDesc"
+                FROM ORDR T0
+                INNER JOIN NNM1 T1 ON T0."Series" = T1."Series"
+                LEFT JOIN RDR1 T2 ON T0."DocEntry" = T2."DocEntry"
+                WHERE T0."DocNum" = ${DocNum}
+                ORDER BY T0."DocNum" DESC
+            `),
+    
+            this.hanaService.query(`
+                SELECT
+                    T0."Address",
+                    T0."Street"
+                FROM CRD1 T0
+                INNER JOIN ORDR T1 ON T0."CardCode" = T1."CardCode"
+                WHERE T1."DocNum" = ${DocNum}
+            `)
+        ]);
         
         if(Array.isArray(saleOrder) && saleOrder.length) {
             return {
